52b29ff22855972ddbd3a6887efd8eb7ad1da07e
jjhkie <83710866+jjhkie@users.noreply.github.com>
2022-09-09 15:38:54 +0900

    [ 기본 UI 생성 ] Main List View & List Detail View UI 생성

---
+++ Brewery/Beer.swift
@@ -0,0 +1,30 @@
+
+import Foundation
+
+struct Beer: Decodable{
+    let id: Int?
+    let name, taglineString, description, brewersTips, imageURL: String?
+    let foodParing: [String]?
+    
+    ///받아온 정보를 해쉬태그형태로 구성해준다.
+    /// tag. hi. bye --> #tag #hi #bye
+    
+    var tagLIne: String {
+        let tags = taglineString?.components(separatedBy: ". ")
+        let hashtags = tags?.map{
+            "#" + $0.replacingOccurrences(of: " ", with: "")
+                .replacingOccurrences(of: ".", with: "")
+                .replacingOccurrences(of: ",", with: " #")
+        }
+        
+        return hashtags?.joined(separator: " ") ?? "" // #tag #hi # bye
+    }
+    
+    enum CodingKeys: String, CodingKey{
+        case id, name, description
+        case taglineString = "tagline"
+        case imageURL = "image_url"
+        case brewersTips = "brewers_tips"
+        case foodParing = "food_paring"
+    }
+}
